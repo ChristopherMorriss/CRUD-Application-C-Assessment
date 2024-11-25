0582a1ab25c8e8e4c94f872a16782705ca0c4ac1
ChristopherMorriss <chrismorriss711@gmail.com>
2024-11-25 14:52:51 +0000

    Updated controller and company repository

--- CRUD Application C# Assessment/Controllers/CompaniesController.cs
+++ CRUD Application C# Assessment/Controllers/CompaniesController.cs
@@ -3,23 +3,25 @@ using CRUD_Application_C__Assessment.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using System.Net;
-//using System.Web.Mvc;
+
 
 namespace CRUD_Application_C__Assessment.Controllers
 {
     public class CompaniesController : Controller
     {
-        //private CompaniesRepository _companiesRepository;
+        private CompaniesRepository _companiesRepository;
 
-        //public CompaniesController()
-        //{
-        //    _companiesRepository = new CompaniesRepository();
-        //}
-
-        public IActionResult CompaniesList() //Viewbag works but the transition to model doesn't
-        //public IActionResult CompaniesList(int id)   
+        public CompaniesController()
         {
-            //var companyModel = _companiesRepository.GetCompany(id);
+            _companiesRepository = new CompaniesRepository();
+        }
+
+        //public IActionResult CompaniesList() //Viewbag works but the transition to model doesn't
+        public IActionResult CompaniesList(int id)   
+        {
+
+            //System.Diagnostics.Debug.WriteLine("Testing");
+            var companyModel = _companiesRepository.GetCompany(id);
             //var companyModel = new CompaniesModel()
             //{
             //    Id = 1,
@@ -28,17 +30,19 @@ namespace CRUD_Application_C__Assessment.Controllers
             //    Logo = "Logo.png",
             //    Website = "www.genericwebsite.com"
             //};
-            ViewBag.Name = "Company Ltd";
-            ViewBag.Email = "email@email.com";
-            ViewBag.Logo = "Logo.png";
-            ViewBag.Website = "www.genericwebsite.com";
-            return View();
-            //return View(companyModel);
+            //ViewBag.Name = "Company Ltd";
+            //ViewBag.Email = "email@email.com";
+            //ViewBag.Logo = "Logo.png";
+            //ViewBag.Website = "www.genericwebsite.com";
+            //return View();
+            //System.Diagnostics.Debug.WriteLine(companyModel.Email); //Evaluates to the correct value
+            //System.Diagnostics.Debug.WriteLine(companyModel); //Evaluates to CRUD_Application_C__Assessment.Models.CompaniesModel
+            return View(companyModel); //Contains model info here but it disappears when passed to the view
         }
 
         public IActionResult CompanyEdit(int? id)
         {
-
+            
             return View();
         }
 
@@ -53,12 +57,10 @@ namespace CRUD_Application_C__Assessment.Controllers
         }
 
         [HttpPost]
+        //public IActionResult CompanyCreate(string name, string email, string logo, string website)
         public IActionResult CompanyCreate(string name, string email, string logo, string website)
         {
-            ViewBag.Name = name;
-            ViewBag.Email = email;
-            ViewBag.Logo = logo;
-            ViewBag.Website = website;
+
             return View();
             
         }
--- CRUD Application C# Assessment/Models/CompaniesModel.cs
+++ CRUD Application C# Assessment/Models/CompaniesModel.cs
@@ -4,11 +4,10 @@ namespace CRUD_Application_C__Assessment.Models
     public class CompaniesModel
     {
         public int Id { get; set; }
-        public  string Name { get; set; } 
+        public required string Name { get; set; } 
         public  string Email { get; set; }
         public  string Logo { get; set; }
         public  string Website { get; set; }
 
-        
     }
 }
