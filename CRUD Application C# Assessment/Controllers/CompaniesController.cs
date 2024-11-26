81eab8305641ae872684aee8be92e5aab03a67f2
ChristopherMorriss <chrismorriss711@gmail.com>
2024-11-26 12:39:00 +0000

    Create company form method is now strongly typed

--- CRUD Application C# Assessment/Controllers/CompaniesController.cs
+++ CRUD Application C# Assessment/Controllers/CompaniesController.cs
@@ -57,16 +57,29 @@ namespace CRUD_Application_C__Assessment.Controllers
 
         public IActionResult CompanyCreate()
         {
-            return View();
+            var companiesModel = new CompaniesModel() //This is used to display the default values for a new company
+            {
+                Name = ""
+            };
+            return View(companiesModel);
         }
-
         [HttpPost]
-        public IActionResult CompanyCreate(string name, string email, string logo, string website)
+        public IActionResult CompanyCreate(CompaniesModel companiesModel)
         {
-
-            return View();
-
+            //if (ModelState.isValid)
+            //{
+            //    _companiesRepository.createCompany(companiesModel)
+            //}
+            return View(companiesModel);
         }
+
+        //[HttpPost]
+        //public IActionResult CompanyCreate(string name, string email, string logo, string website)
+        //{
+            
+        //    return View();
+
+        //}
     }
     //System.Diagnostics.Debug.WriteLine("Testing");
 }
